06fbe8b4b4a8a698658bb46829c4da9cc380fb5a
John pan <s3536515@student.rmit.edu.au>
2017-03-23 17:20:11 +1100

    Update Product.cs

--- A1/Product.cs
+++ A1/Product.cs
@@ -8,10 +8,11 @@ namespace WebA1
 {
    public class Product
     {
+        protected const int defaultRestocking = 4;
+        protected int Threshold = 3;
         public int ID { get; set; }
         public string Name { get; set; }
         public Int32 CurrentStock { get; set; }
         public bool ReStock { get; set; }
-        
     }
 }
